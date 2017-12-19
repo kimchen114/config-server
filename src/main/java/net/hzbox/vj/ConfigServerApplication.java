3b21826a479cd9a276fcf5d78b8a52d9af0faf35
陈金伟 <380328854@qq.com>
2017-12-19 16:45:01 +0800

    ..

---
+++ src/main/java/net/hzbox/vj/ConfigServerApplication.java
@@ -0,0 +1,20 @@
+package net.hzbox.vj;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+	public static void main(String[] args) {
+	    
+	    new SpringApplicationBuilder(ConfigServerApplication.class).web(true).run(args);
+//			SpringApplication.run(ConfigServerApplication.class, args);
+			
+			
+			
+		}
+}
